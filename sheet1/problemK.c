48080b70a58c993b509662af3a57faa04b817cbc
Tisha2607 <murshidatisha2004@gmail.com>
2026-01-21 10:22:58 +0600

    Update problemK.c

---
+++ sheet1/problemK.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main ()
+{
+     int A,B,C,min,max;
+     scanf("%d %d %d", &A,&B,&C);
+     max = min = A;
+     if(B>max)
+     {
+          max=B;
+     }
+     if(C>max)
+     {
+          max=C;
+     }
+     if(B<min)
+     {
+       min=B;
+     }
+     if(C<min)
+     {
+          min=C;
+     }
+     printf("%d %d", min , max);
+ 
+}
