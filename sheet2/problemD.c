8148c092a6a6463c7f7a0576567e0c829a062985
Tisha2607 <murshidatisha2004@gmail.com>
2026-01-21 10:26:53 +0600

    Update problemD.c

---
+++ sheet2/problemD.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    int X;
+ 
+    while (1) {
+        scanf("%d", &X);
+        if (X == 1999) {
+            printf("Correct\n");
+            break;  
+        } else {
+            printf("Wrong\n");
+        }
+    }
+ 
+    return 0;
+}
